595e1c51a671b1ba0c438abf3f8c88d5f0badb72
Sabin Thomas <thomasmsabin@gmail.com>
2018-03-27 12:18:14 -0400

    Changes made to accommodate deploying on Azure.

--- k8/run.sh
+++ k8/run.sh
@@ -1,18 +1,53 @@
+# Configure default group
+    az group create -n 'sandbox02' -l 'eastus'
+    az configure --defaults group='sandbox02'
+
+# set up the Azure storage class
+az storage account create --resource-group MC_sandbox01_nodebbcluster01_eastus --name nodebbcluster01storage --location eastus --sku Standard_LRS    
+
+# Azure Container registry
+    # set up azure container registry
+    az acr create --name sandbox02registry --sku Basic
+
+    # login to the container reg
+    az acr login --name sandbox02registry
+
+    # get the loginserver
+    az acr list --query "[].{acrLoginServer:loginServer}" --output table
+
+    # build the image
+    cd ../
+    docker build -t sandbox02registry.azurecr.io/nodebb:v1 .
+    
+    # push to the registry
+    docker push sandbox02registry.azurecr.io/nodebb:v1
+
+    # list all the images
+    az acr repository list -n sandbox02registry
+
+    # link AKS with ACS, give role assignment
+    CLIENT_ID=$(az aks show --name nodebbcluster01 --query "servicePrincipalProfile.clientId" --output tsv)
+    ACR_ID=$(az acr show --name sandbox02registry --query "id" --output tsv)
+    # remove default group before you do az role assignment
+    az configure defaults --defaults group=''
+    az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
+
 # set up the namespace
 kubectl create -f namespace.yaml
 
-#set up local volumes
-kubectl create -f local-volumes.yaml
-kubectl get pv
+#set up storage account
+kubectl create -f azure-storage.yaml
 
-# Replace BLUEFYRE_AGENT_ID with the API key from the Bluefyre app
-kubectl create secret generic bluefyre-agent-id --from-literal=agentid=BLUEFYRE_AGENT_ID --namespace=nodebb
+# secrets
 
-# set your default admin username and password
-kubectl create secret generic nodebbadminusername --from-literal=nodebbadminusername=admin --namespace=nodebb
-kubectl create secret generic nodebbadminpassword --from-literal=nodebbadminpassword=REPLACEME --namespace=nodebb
+    # Replace BLUEFYRE_AGENT_ID with the API key from the Bluefyre app
+    kubectl create secret generic bluefyre-agent-id --from-literal=agentid=BLUEFYRE_AGENT_ID --namespace=nodebb
 
-kubectl get secrets --namespace=nodebb
+    # set your default admin username and password
+    kubectl create secret generic nodebbadminusername --from-literal=nodebbadminusername=admin --namespace=nodebb
+    kubectl create secret generic nodebbadminpassword --from-literal=nodebbadminpassword=REPLACEME --namespace=nodebb
+
+    kubectl get secrets --namespace=nodebb
 
 # set up the mongo service, pvclaim, deployment
 kubectl create -f mongo.yaml --namespace=nodebb
@@ -28,17 +63,28 @@ kubectl create -f nodebb-ingress.yaml -n nodebb
 kubectl create -f nodebb-ingress-sticky.yaml -n nodebb
 
 
+# get current status
+kubectl get svc,deployment,rc,job,pvc,pv,secret,po 
+
+
 # clear everything
-kubectl delete service,deployment,replicationcontroller,job,persistentvolumeclaim,secret --all --namespace=nodebb
+kubectl delete svc,deployment,rc,job,pvc,pv,secret,po --all --namespace=nodebb
 
 # clear ingress controller
 kubectl delete ingress nodebb-ingress
 
 # clear just infrastructure without secrets
-kubectl delete service,deployment,replicationcontroller,job,persistentvolumeclaim --all --namespace=nodebb
+kubectl delete svc,deployment,rc,job,pv,pvc --all --namespace=nodebb
 
 # clear just the app
-kubectl delete service,deployment -l name=web --namespace=nodebb
+kubectl delete svc,deployment -l name=web --namespace=nodebb
 
 # clear the setup job
 kubectl delete job -l app=nodebb-init --namespace=nodebb
+
+
+
+# clear mongo claim
+kubectl delete service,pvc,rc -l name=mongo
+kubectl delete pvc -l name="mongo-pv-claim"
+
