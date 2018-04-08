15276b28e123c8b4a07917b75aecbe17dbb0a0bb
Sabin Thomas <thomasmsabin@gmail.com>
2018-04-08 16:28:59 -0400

    remove scripts to set up the storage account

--- k8/run.sh
+++ k8/run.sh
@@ -2,8 +2,8 @@
     az group create -n 'sandbox02' -l 'eastus'
     az configure --defaults group='sandbox02'
 
-# set up the Azure storage class
-az storage account create --resource-group MC_sandbox01_nodebbcluster01_eastus --name nodebbcluster01storage --location eastus --sku Standard_LRS    
+# set up the Azure storage class - we can skip this
+# az storage account create --resource-group MC_sandbox02_nodebbcluster01_eastus --name nodebbcluster01storage --location eastus --sku Standard_LRS    
 
 # Azure Container registry
     # set up azure container registry
@@ -25,18 +25,21 @@ az storage account create --resource-group MC_sandbox01_nodebbcluster01_eastus -
     # list all the images
     az acr repository list -n sandbox02registry
 
-    # link AKS with ACS, give role assignment
-    CLIENT_ID=$(az aks show --name nodebbcluster01 --query "servicePrincipalProfile.clientId" --output tsv)
-    ACR_ID=$(az acr show --name sandbox02registry --query "id" --output tsv)
-    # remove default group before you do az role assignment
-    az configure defaults --defaults group=''
-    az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
+    az acr repository show-tags --name sandbox02registry --repository nodebb
+
+# link AKS with ACS, give role assignment
+CLIENT_ID=$(az aks show --name nodebbcluster01 --query "servicePrincipalProfile.clientId" --output tsv)
+ACR_ID=$(az acr show --name sandbox02registry --query "id" --output tsv)
+
+# remove default group before you do az role assignment
+az configure defaults --defaults group=''
+az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
 
 # set up the namespace
 kubectl create -f namespace.yaml
 
 #set up storage account
-kubectl create -f azure-storage.yaml
+# kubectl create -f azure-storage.yaml
 
 # secrets
 
@@ -64,7 +67,10 @@ kubectl create -f nodebb-ingress-sticky.yaml -n nodebb
 
 
 # get current status
-kubectl get svc,deployment,rc,job,pvc,pv,secret,po 
+kubectl get svc,deployment,rc,job,pvc,pv,secret,po  -w
+
+# get the loadbalanced ip
+kubectl get svc -n kube-system -l app=nginx-ingress -l component=controller
 
 
 # clear everything
